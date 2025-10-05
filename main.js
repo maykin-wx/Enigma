61268bb3b7548205022707eb02cf14b8dad9573d
Maykon Mendes <maykon.mendes40@gmail.com>
2025-10-05 19:57:25 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -9,7 +9,7 @@ document.addEventListener('DOMContentLoaded', () => {
     const RESPOSTA_NUMERO = "1915132181"; 
     
     // Define o URL de destino
-    const URL_DESTINO = "mantab.html";
+    const URL_DESTINO = "mantab";
 
     // 2. Adiciona o listener de evento para a submissão do formulário
     form.addEventListener('submit', function(event) {
